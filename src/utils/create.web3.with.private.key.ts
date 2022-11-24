f8cfe940b0b4c65f5a4fb39297005c025e060866
mncdg <mncdg@ukr.net>
2022-11-24 18:08:17 +0200

    undo web3

--- src/utils/create.web3.with.private.key.ts
+++ src/utils/create.web3.with.private.key.ts
@@ -1,11 +1,7 @@
 import Web3 from "web3";
 
-const web3HttpTimeout = parseInt(process.env.WEB_HTTP_TIMEOUT || '') || 60 * 1000; //1min
-
 export function createWeb3WithPrivateKey(rpc: string, privateKey: string) {
-  const web3 = new Web3(new Web3.providers.HttpProvider(rpc, {
-    timeout: web3HttpTimeout,
-  }));
+  const web3 = new Web3(rpc);
   const accountEvmFromPrivateKey =
     web3.eth.accounts.privateKeyToAccount(privateKey);
   web3.eth.accounts.wallet.add(accountEvmFromPrivateKey);
