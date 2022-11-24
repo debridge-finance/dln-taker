c263b6f84fca0b6f0f5b67e6dd80745e86b6ef88
mncdg <mncdg@ukr.net>
2022-11-24 18:27:36 +0200

    Add WhiteListedReceiver

---
+++ src/validators/white.listed.receiver.ts
@@ -0,0 +1,42 @@
+import { ChainId, OrderData } from "@debridge-finance/dln-client";
+
+import { helpers } from "@debridge-finance/solana-utils";
+
+import { ExecutorConfig } from "../config";
+
+import { ValidatorContext } from "./order.validator";
+import { OrderValidatorInterface } from "./order.validator.interface";
+
+import { convertAddressToBuffer } from "../utils/convert.address.to.buffer";
+import { buffersAreEqual } from "../utils/buffers.are.equal";
+
+/**
+ * Checks if the address who is a receiver the order on the take chain is in the whitelist. This validator is useful to filter out orders placed by the trusted parties.
+ */
+export class WhiteListedReceiver extends OrderValidatorInterface {
+
+  private addressesBuffer: Uint8Array[];
+
+  constructor(private readonly addresses: string[]) {
+    super();
+  }
+
+  init(chainId: ChainId): Promise<void> {
+    super.chainId = chainId;
+    this.addressesBuffer = this.addresses.map((address) => convertAddressToBuffer(chainId, address));
+    return Promise.resolve();
+  }
+
+  validate(order: OrderData, config: ExecutorConfig, context: ValidatorContext): Promise<boolean> {
+    const logger = context.logger.child({ validator: "WhiteListedReceiver" });
+    const result = this.addressesBuffer.some(address => buffersAreEqual(order.receiver, address))
+
+    const receiver = helpers.bufferToHex(Buffer.from(order.receiver));
+    logger.info(`approve status: ${result}, receiver ${receiver}`);
+    return Promise.resolve(result);
+  }
+}
+
+export function whiteListedReceiver(addresses: string[]): OrderValidatorInterface {
+  return new WhiteListedReceiver(addresses)
+}
