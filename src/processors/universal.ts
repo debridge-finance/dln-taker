0b1911d645922925350493e4342b2e844cea7b5e
mncdg <mncdg@ukr.net>
2023-01-18 12:08:18 +0100

    delete useless checking isBatchUnlockLocked

--- src/processors/universal.ts
+++ src/processors/universal.ts
@@ -397,7 +397,6 @@ class UniversalProcessor extends BaseOrderProcessor {
 
     // execute batch unlock processing for full batch
     if (orderIds.size >= this.params.batchUnlockSize) {
-      if (!this.isBatchUnlockLocked) this.isBatchUnlockLocked = true;
       this.processUnlockBatches(giveChain, context);
     }
   }
