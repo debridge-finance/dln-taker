e6c4144babcf6afd41673c0d3971aaa363660dee
mncdg <mncdg@ukr.net>
2023-04-10 18:11:45 +0200

    add new handlers

---
+++ src/hooks/notification/TelegramNotifier.ts
@@ -0,0 +1,24 @@
+import { NotificationContext, Notifier } from "./Notifier";
+import axios from "axios";
+
+export class TelegramNotifier extends Notifier {
+  constructor(
+      private readonly tgKey: string,
+      private readonly tgChatIds: string[]
+  ) {
+    super();
+  }
+
+  async notify(message: string, context: NotificationContext): Promise<void> {
+    const logger = context.logger.child({
+      notification: TelegramNotifier.name,
+    });
+    for (const chatId of this.tgChatIds) {
+      await axios.post(`https://api.telegram.org/bot${this.tgKey}/sendMessage`, {
+        chat_id: chatId,
+        text: message,
+      });
+      logger.debug(`Notification ${message} is sent`);
+    }
+  }
+}
