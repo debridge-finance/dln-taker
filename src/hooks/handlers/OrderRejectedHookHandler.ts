33c09f2c02addcb7d527c6e6929325b6430a76c5
mncdg <mncdg@ukr.net>
2023-04-11 11:22:14 +0200

    fix comments

--- src/hooks/handlers/OrderRejectedHookHandler.ts
+++ src/hooks/handlers/OrderRejectedHookHandler.ts
@@ -2,7 +2,7 @@ import { HookHandler } from "../HookHandler";
 import {Notifier} from "../notification/Notifier";
 import {Hooks, RejectionReason} from "../HookEnums";
 import {HookParams} from "../types/HookParams";
-import {tokenAddressToString} from "@debridge-finance/dln-client";
+import {ChainId, tokenAddressToString} from "@debridge-finance/dln-client";
 import BigNumber from "bignumber.js";
 import Web3 from "web3";
 
@@ -18,13 +18,15 @@ export const orderRejected = (
             return;
         }
         const order = arg.order.order;
-        const [giveDecimals, takeDecimals] = await Promise.all([
+        const [giveDecimals, takeDecimals, giveTokenSymbol, takeTokenSymbol] = await Promise.all([
             arg.context.config.client.getDecimals(order.give.chainId, order.give.tokenAddress, arg.context.giveChain.fulfillProvider.connection as Web3),
             arg.context.config.client.getDecimals(order.take.chainId, order.take.tokenAddress, arg.context.config.chains[order.take.chainId]?.fulfillProvider.connection as Web3),
+            arg.context.config.client.getTokenSymbol(order.give.chainId, order.give.tokenAddress, arg.context.giveChain.fulfillProvider.connection as Web3),
+            arg.context.config.client.getTokenSymbol(order.take.chainId, order.take.tokenAddress, arg.context.config.chains[order.take.chainId]?.fulfillProvider.connection as Web3),
         ]);
-        const giveInfo = `giveInfo(chainId=${order.give.chainId}, tokenAddress=${tokenAddressToString(order.give.chainId, order.give.tokenAddress)}, amount=${new BigNumber(order.give.amount.toString()).div(new BigNumber(10).pow(giveDecimals))})`;
-        const takeInfo = `takeInfo(chainId=${order.take.chainId}, tokenAddress=${tokenAddressToString(order.take.chainId, order.take.tokenAddress)}, amount=${new BigNumber(order.take.amount.toString()).div(new BigNumber(10).pow(takeDecimals))})`;
-        const message = `Order #<a href="https://dln.debridge.finance/order?orderId=${arg.order.orderId}">${arg.order.orderId}</a>(attempt: ${arg.attempts})(${giveInfo} ${takeInfo}) has been rejected because of ${ RejectionReason[arg.reason] }: ${ arg.message }`
+        const giveInfo = `${new BigNumber(order.give.amount.toString()).div(new BigNumber(10).pow(giveDecimals))} ${ChainId[order.give.chainId]}@${giveTokenSymbol}`;
+        const takeInfo = `${new BigNumber(order.take.amount.toString()).div(new BigNumber(10).pow(takeDecimals))} ${ChainId[order.take.chainId]}@${takeTokenSymbol}`;
+        const message = `Order #<a href="https://dln.debridge.finance/order?orderId=${arg.order.orderId}">${arg.order.orderId}</a>(attempt: ${arg.attempts})(${giveInfo} -> ${takeInfo}) has been rejected because of ${ RejectionReason[arg.reason] }: ${ arg.message }`
         await notifier.notify(message, { logger });
     };
 };
