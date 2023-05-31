9d8a3b95edbcc6da494ebe04a03ccb414f15f5cf
mncdg <mncdg@ukr.net>
2023-05-31 13:07:29 +0200

    WIP: telegram notification plugins

---
+++ src/hooks/notification/TelegramNotifier.ts
@@ -0,0 +1,25 @@
+import { NotificationContext, Notifier } from "./Notifier";
+import axios from "axios";
+
+export class TelegramNotifier extends Notifier {
+  constructor(
+      private readonly tgKey: string,
+      private readonly tgChatIds: string[]
+  ) {
+    super();
+  }
+
+  async notify(message: string, context: NotificationContext): Promise<void> {
+    const logger = context.logger.child({
+      notification: TelegramNotifier.name,
+    });
+    for (const chatId of this.tgChatIds) {
+      await axios.post(`https://api.telegram.org/bot${this.tgKey}/sendMessage`, {
+        chat_id: chatId,
+        text: message,
+        parse_mode: "HTML"
+      });
+      logger.debug(`Sent notification: ${JSON.stringify(message)}`);
+    }
+  }
+}
---
+++ src/hooks/notification/params/TelegramNotificationParams.ts
@@ -0,0 +1,3 @@
+export class TelegramNotificationParams {
+  chatIds: string[];
+}
